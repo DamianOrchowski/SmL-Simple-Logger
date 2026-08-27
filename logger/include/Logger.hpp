13d6fcc01ac7009cc2414d31ecf458ae4ef5176d
Damian Orchowski <orchowskidamian@protonmail.com>
2026-08-27 12:46:21 +0200

    Added creating file

--- logger/include/Logger.hpp
+++ logger/include/Logger.hpp
@@ -5,7 +5,9 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <iostream>
 #include <filesystem>
+#include <string>
 
 namespace fs = std::filesystem;
 
@@ -14,14 +16,51 @@ class Logger
 private:
     static Logger *_instance;
 
-    const char* _logFileName;
+    std::string _logFileOpeningLine;
+
+    const char *_logFileName;
     std::fstream _logFile;
 
-    const char* format_file_name() {
+    fs::path _logsPath = fs::path("logs/");
+
+    struct tm get_time()
+    {
         time_t timestamp = time(&timestamp);
         struct tm now = *localtime(&timestamp);
+        return now;
+    }
 
-        
+    const char *format_file_name(struct tm now)
+    {
+        char buffer[80];
+        std::strftime(buffer, sizeof(buffer), "%d-%m-%Y %H_%M_%S.log", &now);
+        return buffer;
+    }
+
+    void create_log_file()
+    {
+        struct tm now = get_time();
+        _logFileName = format_file_name(now);
+        if (!fs::exists(_logsPath) || !fs::is_directory(_logsPath))
+        {
+            fs::create_directories(_logsPath);
+        }
+        fs::path logFilePath = _logsPath / _logFileName;
+        _logFile.open(logFilePath);
+        if (!_logFile.is_open())
+        {
+            std::cerr << "_logFile with path: " << logFilePath << " failed to open!\n";
+        }
+        char time_buffer[40];
+        std::strftime(time_buffer, sizeof(time_buffer), "%H:%M:%S %d-%m-%Y", &now);
+        _logFile << "======================================\n";
+        _logFile << time_buffer << '\n';
+        if (!_logFileOpeningLine.empty())
+        {
+            _logFile << _logFileOpeningLine << '\n';
+        }
+        _logFile << "============= Logs Start =============\n";
+        _logFile.close();
     }
 
 public:
@@ -43,7 +82,6 @@ public:
     }
 
     void CloseLogFile();
-
 };
 
 #endif
